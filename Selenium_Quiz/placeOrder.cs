1e292d80bfec7711968cbb8ede0a55e65db3bb02
Abdullah Sami <abdullahsami201@gmail.com>
2022-10-06 00:15:40 +0500

    changed

--- Selenium_Quiz/placeOrder.cs
+++ Selenium_Quiz/placeOrder.cs
@@ -15,8 +15,12 @@ namespace Selenium_Quiz
        
         
         By product = By.XPath("(//i[@class='fa fa-shopping-cart'])[2]");
-        By cartbutton1 = By.XPath("(//*[@id='cartModal']/div/div/div[3]/button");
-
+        By contineo = By.XPath("//button[@data-dismiss='modal']");
+        By cartbutton = By.XPath("//div[@class='col-sm-8']/div/ul/li[3]/a");
+        By checkoutbutton = By.XPath("//a[text()='Proceed To Checkout']");
+        By address = By.XPath("//ul[@id='address_delivery']/li[4]");
+        By review = By.XPath("//h2[text()='Review Your Order']");
+        
 
         #endregion
 
@@ -33,19 +37,48 @@ namespace Selenium_Quiz
             
         }
 
-        
+        void clickContineu()
+        {
+            click(contineo);
 
+        }
 
+        void clickCart()
+        {
+            scrollToElement(cartbutton);
+            click(cartbutton);
 
+        }
+
+        void verifyCheckoutButton(string value)
+        {
+            verifyElement(checkoutbutton, value);
+
+        }
+
+        void clickProceed()
+        {
+            
+            click(checkoutbutton);
+
+        }
+        void verifyAddressDetail(string value)
+        {
+            verifyElement(address, value);
+            scrollToElement(review);
+        }
+       
 
         public void placeorder()
         {
             scrollToproduct();
             clickProduct();
             Thread.Sleep(5000);
-            IWebElement state = findElement(cartbutton1);
-            IJavaScriptExecutor commonExecuter = (IJavaScriptExecutor)commonDriver;
-            commonExecuter.ExecuteScript("arguments[0].scrollIntoView(true); ", state);
+            clickContineu();
+            clickCart();
+            verifyCheckoutButton("Proceed To Checkout");
+            clickProceed();
+            verifyAddressDetail("45 -c ,street 1");
 
 
 
